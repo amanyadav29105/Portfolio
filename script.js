16d923cf758acdda22343aebd35be16f89c6043c
AMAN <amanyadav29105@gmail.com>
2026-06-19 15:16:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,82 @@
+const text =
+[
+"Web Developer",
+"CSE Student",
+"Photographer",
+"Creative Designer"
+];
+
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type(){
+
+if(count === text.length){
+count = 0;
+}
+
+currentText = text[count];
+
+letter = currentText.slice(0, ++index);
+
+document.getElementById("typing").textContent = letter;
+
+if(letter.length === currentText.length){
+
+count++;
+index = 0;
+
+setTimeout(type, 1200);
+
+}else{
+
+setTimeout(type, 120);
+
+}
+
+})();
+
 window.addEventListener("scroll", () => {
 
-const header = document.querySelector(".header");
+const reveals =
+document.querySelectorAll(".reveal");
 
-header.style.boxShadow =
-window.scrollY > 50
-? "0 5px 20px rgba(0,0,0,.3)"
-: "none";
+reveals.forEach(item => {
+
+const top =
+item.getBoundingClientRect().top;
+
+if(top < window.innerHeight - 100){
+item.classList.add("active");
+}
 
 });
+
+});
+
+const topBtn =
+document.getElementById("topBtn");
+
+window.onscroll = function(){
+
+if(document.body.scrollTop > 300 ||
+document.documentElement.scrollTop > 300){
+
+topBtn.style.display = "block";
+
+}else{
+
+topBtn.style.display = "none";
+}
+};
+
+topBtn.onclick = function(){
+
+window.scrollTo({
+top:0,
+behavior:"smooth"
+});
+
+};
