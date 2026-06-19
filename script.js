46a94fa418ef59ec9ede18abac4d22bdb3575997
AMAN <amanyadav29105@gmail.com>
2026-06-19 15:37:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,82 +1,8 @@
-const text =
-[
-"Web Developer",
-"CSE Student",
-"Photographer",
-"Creative Designer"
-];
+document.getElementById("contactForm").addEventListener("submit", function(e){
+e.preventDefault();
 
-let count = 0;
-let index = 0;
-let currentText = "";
-let letter = "";
+document.getElementById("success").innerText =
+"Message sent successfully!";
 
-(function type(){
-
-if(count === text.length){
-count = 0;
-}
-
-currentText = text[count];
-
-letter = currentText.slice(0, ++index);
-
-document.getElementById("typing").textContent = letter;
-
-if(letter.length === currentText.length){
-
-count++;
-index = 0;
-
-setTimeout(type, 1200);
-
-}else{
-
-setTimeout(type, 120);
-
-}
-
-})();
-
-window.addEventListener("scroll", () => {
-
-const reveals =
-document.querySelectorAll(".reveal");
-
-reveals.forEach(item => {
-
-const top =
-item.getBoundingClientRect().top;
-
-if(top < window.innerHeight - 100){
-item.classList.add("active");
-}
-
+this.reset();
 });
-
-});
-
-const topBtn =
-document.getElementById("topBtn");
-
-window.onscroll = function(){
-
-if(document.body.scrollTop > 300 ||
-document.documentElement.scrollTop > 300){
-
-topBtn.style.display = "block";
-
-}else{
-
-topBtn.style.display = "none";
-}
-};
-
-topBtn.onclick = function(){
-
-window.scrollTo({
-top:0,
-behavior:"smooth"
-});
-
-};
